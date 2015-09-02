d917fa8a2684334a3ca3b5eb7d401b251d69b207
Nicolas Beaussart <nic.beaussart@gmail.com>
2015-09-02 16:53:21 +0200

    Adding .gitignore

---
+++ src/com/lifeproject/data/Humain.java
@@ -0,0 +1,12 @@
+package com.lifeproject.data;
+
+/**
+ * Created by beaussan on 02/09/15.
+ */
+public class Humain {
+    private boolean isMasculin;
+    private String surname;
+    private String name;
+
+
+}
