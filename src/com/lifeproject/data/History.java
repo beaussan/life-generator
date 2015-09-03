e091dc751d64a966b3abcaed92ee9a6a559aaad3
Raphaël Fitoussi <fitoussr@hevea06.iut-infobio.priv.univ-lille1.fr>
2015-09-03 11:15:58 +0200

    Ajout de compilations d'histoire

---
+++ src/com/lifeproject/data/History.java
@@ -0,0 +1,43 @@
+package com.lifeproject.data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class History {
+	List<HistoryByte> debut = new ArrayList<HistoryByte>();
+
+	public static History getInstance() {
+		if (null == instance) {
+			instance = new History();
+		}
+		return instance;
+	}
+
+	private History() {
+		HistoryByte h1 = new HistoryByte(
+				"Il était une fois l'histoire de {name}, {fils/fille} de noble en quête de gloire et de pouvoir suite à la déchéance qu'a connu{/e} sa famille suite à la révolte de leur peuple.");
+		HistoryByte h2 = new HistoryByte(
+				"Un enfant trouvé sous le cadavre de sa mère, adopté par la femme d'un mercenaire. Néanmoins, elle mourut également par la peste, l'enfant dût alors prendre les armes pour subsister.");
+		HistoryByte h3 = new HistoryByte(
+				"Né{/e} d'une union sans amour d'un bandit et d'une villageoise, l'enfant ne connu pas son père et ne connut que les sévices de sa mère qui voyait en {lui/elle} l'image de son aggresseur, {name} dû fuguer très jeune, {il/elle} rejoignit alors une troupe de bandit et commença à répandre la mort et la terreur dans les villages qu'{il/elle} rencontrait.");
+		debut.add(h1);
+		h1.add(h2);
+		h1.add(h3);
+	}
+
+	public String getHistory(Humain hum){
+    	StringBuilder sb = new StringBuilder();
+    	Random r = new Random();
+    	HistoryByte hb = debut.get(r.nextInt(debut.size()));
+    	do {
+    		sb.append(hb.getStoryFor(hum));
+    		hb = hb.getPossibleOutcome().get(r.nextInt(hb.getPossibleOutcome().size()));
+    	} while(! hb.getPossibleOutcome().isEmpty());
+    	
+    	
+    	return sb.toString();
+    }
+
+	private static History instance;
+}
